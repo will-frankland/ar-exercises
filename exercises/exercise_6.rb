7283158aaaaba53d91d3d5a79f8d3076af75a033
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-05-03 15:39:54 -0400

    exercise 7 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Will", last_name: "Frankland", hourly_rate: 100)
+@store2.employees.create(first_name: "Lewis", last_name: "Hamilton", hourly_rate: 25)
+@store1.employees.create(first_name: "Joel", last_name: "Zimmerman", hourly_rate: 250)
+@store1.employees.create(first_name: "Thiago", last_name: "Silva", hourly_rate: 45)
+@store2.employees.create(first_name: "Steven", last_name: "Gerrard", hourly_rate: 90)
+@store1.employees.create(first_name: "Scottie", last_name: "Barnes", hourly_rate: 125)
+@store2.employees.create(first_name: "Bruce", last_name: "McLaren", hourly_rate: 70)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "Enter store name: "
+@store_name = gets.chomp
+
+Store.create(name: @store_name)
+puts @store_name.errors.full_messages
