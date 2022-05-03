fe70875f99afca8ed16f35fc43b7183f490f7b70
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-05-03 14:46:30 -0400

    exercise 5 complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -10,3 +10,13 @@ puts "----------"
 store = Store.create(id: 4, name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
 store = Store.create(id: 5, name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 store = Store.create(id: 6, name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |element|
+  puts "Name: #{element.name}, Annual Revenue: #{element.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel = true and annual_revenue < 1000000")
+@womens_stores.each do |element|
+  puts "Name: #{element.name}, Annual Revenue: #{element.annual_revenue}"
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@sum = Store.sum(:annual_revenue)
+puts @sum
+
+@average =Store.average(:annual_revenue)
+puts @average
+
+@overMill = Store.where("annual_revenue > 1000000").count
+puts @overMill
